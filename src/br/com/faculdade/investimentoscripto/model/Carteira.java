4d4ce6159dd3f7ad3ce9887277c806b2c569a71e
Guizinhn <gugui4020@gmail.com>
2026-04-17 20:40:27 -0300

    fiz as classes empresa e carteira que faltavam

--- src/br/com/faculdade/investimentoscripto/model/Carteira.java
+++ src/br/com/faculdade/investimentoscripto/model/Carteira.java
@@ -1,1 +1,40 @@
-//guilheme
+package br.com.faculdade.investimentocripto.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carteira {
+    private Long id;
+    private BigDecimal saldoTotalGeral;
+    private List<Transacao> listaTransacoes;
+    private List<AtivoCripto> listaAtivos;
+
+    public Carteira(Long id) {
+        this.id = id;
+        this.saldoTotalGeral = BigDecimal.ZERO;
+        this.listaTransacoes = new ArrayList<>();
+        this.listaAtivos = new ArrayList<>();
+    }
+
+    public void registrarTransacao(Transacao t) {
+        listaTransacoes.add(t);
+
+        if (t.getValorTotal() != null) {
+            this.saldoTotalGeral = this.saldoTotalGeral.add(t.getValorTotal());
+        }
+    }
+
+    public AtivoCripto buscarAtivo(String ticker) {
+        for (AtivoCripto a : listaAtivos) {
+            if (a.getTicker().equalsIgnoreCase(ticker)) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public BigDecimal getSaldoTotalGeral() {
+        return saldoTotalGeral;
+    }
+}
