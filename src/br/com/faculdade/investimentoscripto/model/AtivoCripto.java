da2f2c5842ca48e699be0d89a8865b1559cee8be
Pedro <pedrovitorbaier@outlook.com>
2026-04-17 23:59:49 -0300

    feat: adiciona classes AtivoCripto e Transacao

--- src/br/com/faculdade/investimentoscripto/model/AtivoCripto.java
+++ src/br/com/faculdade/investimentoscripto/model/AtivoCripto.java
@@ -1,1 +1,69 @@
-//pedro
+package br.com.faculdade.investimentoscripto.model;
+
+public class AtivoCripto {
+
+    private String simbolo;
+    private String nome;
+    private double cotacaoAtual;
+    private double variacao24h;
+
+    public AtivoCripto() {
+    }
+
+    public AtivoCripto(String simbolo, String nome, double cotacaoAtual, double variacao24h) {
+        this.simbolo = simbolo;
+        this.nome = nome;
+        this.cotacaoAtual = cotacaoAtual;
+        this.variacao24h = variacao24h;
+    }
+
+    public void atualizarCotacao(double novaCotacao) {
+        this.variacao24h = ((novaCotacao - this.cotacaoAtual) / this.cotacaoAtual) * 100;
+        this.cotacaoAtual = novaCotacao;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("=== Ativo Cripto ===");
+        System.out.println("Simbolo: " + simbolo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Cotacao Atual: R$ " + String.format("%.2f", cotacaoAtual));
+        System.out.println("Variacao 24h: " + String.format("%.2f", variacao24h) + "%");
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCotacaoAtual() {
+        return cotacaoAtual;
+    }
+
+    public void setCotacaoAtual(double cotacaoAtual) {
+        this.cotacaoAtual = cotacaoAtual;
+    }
+
+    public double getVariacao24h() {
+        return variacao24h;
+    }
+
+    public void setVariacao24h(double variacao24h) {
+        this.variacao24h = variacao24h;
+    }
+
+    @Override
+    public String toString() {
+        return simbolo + " - " + nome + " | R$ " + String.format("%.2f", cotacaoAtual);
+    }
+}
--- src/br/com/faculdade/investimentoscripto/model/Transacao.java
+++ src/br/com/faculdade/investimentoscripto/model/Transacao.java
@@ -1,1 +1,90 @@
-//pedro
+package br.com.faculdade.investimentoscripto.model;
+
+public class Transacao {
+
+    private int id;
+    private String tipo;
+    private double valor;
+    private String dataHora;
+    private String descricao;
+    private String simboloAtivo;
+
+    public Transacao() {
+    }
+
+    public Transacao(int id, String tipo, double valor, String dataHora, String descricao, String simboloAtivo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.valor = valor;
+        this.dataHora = dataHora;
+        this.descricao = descricao;
+        this.simboloAtivo = simboloAtivo;
+    }
+
+    public void registrar() {
+        System.out.println("Transacao registrada com sucesso!");
+        System.out.println("ID: " + id);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Ativo: " + simboloAtivo);
+        System.out.println("Valor: R$ " + String.format("%.2f", valor));
+        System.out.println("Data/Hora: " + dataHora);
+        System.out.println("Descricao: " + descricao);
+    }
+
+    public String getResumo() {
+        return "[" + dataHora + "] " + tipo + " | " + simboloAtivo + " | R$ " + String.format("%.2f", valor);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getSimboloAtivo() {
+        return simboloAtivo;
+    }
+
+    public void setSimboloAtivo(String simboloAtivo) {
+        this.simboloAtivo = simboloAtivo;
+    }
+
+    @Override
+    public String toString() {
+        return getResumo();
+    }
+}
