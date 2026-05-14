6d2c26389c595d9b53cde8604003536d1a04a73a
Guizinhn <gugui4020@gmail.com>
2026-05-13 21:21:43 -0300

    Fase 3: Encapsulamento, Getters e Setters na Carteira e Empresa

--- src/br/com/faculdade/investimentoscripto/model/Carteira.java
+++ src/br/com/faculdade/investimentoscripto/model/Carteira.java
@@ -10,8 +10,16 @@ public class Carteira {
     private List<Transacao> listaTransacoes;
     private List<AtivoCripto> listaAtivos;
 
+
     public Carteira(Long id) {
         this.id = id;
+        this.saldoTotalGeral = BigDecimal.ZERO;
+        this.listaTransacoes = new ArrayList<>();
+        this.listaAtivos = new ArrayList<>();
+    }
+
+
+    public Carteira() {
         this.saldoTotalGeral = BigDecimal.ZERO;
         this.listaTransacoes = new ArrayList<>();
         this.listaAtivos = new ArrayList<>();
@@ -34,7 +42,36 @@ public class Carteira {
         return null;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public BigDecimal getSaldoTotalGeral() {
         return saldoTotalGeral;
+    }
+
+    public void setSaldoTotalGeral(BigDecimal saldoTotalGeral) {
+        this.saldoTotalGeral = saldoTotalGeral;
+    }
+
+    public List<Transacao> getListaTransacoes() {
+        return listaTransacoes;
+    }
+
+    public void setListaTransacoes(List<Transacao> listaTransacoes) {
+        this.listaTransacoes = listaTransacoes;
+    }
+
+    public List<AtivoCripto> getListaAtivos() {
+        return listaAtivos;
+    }
+
+    public void setListaAtivos(List<AtivoCripto> listaAtivos) {
+        this.listaAtivos = listaAtivos;
     }
 }
--- src/br/com/faculdade/investimentoscripto/model/Empresa.java
+++ src/br/com/faculdade/investimentoscripto/model/Empresa.java
@@ -10,10 +10,16 @@ public class Empresa {
     private String cnpj;
     private List<Carteira> carteiras;
 
+
     public Empresa(Long id, String razaoSocial, String cnpj) {
         this.id = id;
         this.razaoSocial = razaoSocial;
         this.cnpj = cnpj;
+        this.carteiras = new ArrayList<>();
+    }
+
+
+    public Empresa() {
         this.carteiras = new ArrayList<>();
     }
 
@@ -30,7 +36,37 @@ public class Empresa {
         this.carteiras.add(c);
     }
 
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getRazaoSocial() {
         return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public List<Carteira> getCarteiras() {
+        return carteiras;
+    }
+
+    public void setCarteiras(List<Carteira> carteiras) {
+        this.carteiras = carteiras;
     }
 }
